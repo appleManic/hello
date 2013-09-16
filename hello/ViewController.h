f97a572fbb39abbdd9d92b22ae50871d2934bfc0
appleManic <user@Duys-MacBook-Air.local>
2013-09-15 20:05:30 -0400

    THis is the first commit

---
+++ hello/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  hello
+//
+//  Created by Techgene01 on 7/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
